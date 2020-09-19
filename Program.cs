a28e54d2bb40bcd4ed299c39b3d9754861a91f62
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-19 19:08:47 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace olaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              string nome;
+
+            //<---Inicio(Digite--->//
+            Console.Write("Digite Seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"Olá, {nome}!");
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+            
+        }
+    }
+}
